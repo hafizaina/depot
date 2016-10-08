e8b8bf61cc97e06534b77cce8f903bc9c9ad88e6
Hafiz Aina <hafizaina94@gmail.com>
2016-10-08 01:37:59 -0400

    Populated some basic unit tests for the product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  # We are validating that the image_url is in gif, jpg, or png format via regular expression
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\. (gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG, or PNG image'
+  }
+
 end
